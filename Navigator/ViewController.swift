dbb74fcad10754498ea17ba4889c1b37f2f33e0f
Arthithai Aamlid <osacn_arthithai.aamlid@krungthai.com>
2023-02-24 14:21:56 +0700

    Initial Commit

---
+++ Navigator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigator
+//
+//  Created by arthithai.aamlid on 24/2/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
